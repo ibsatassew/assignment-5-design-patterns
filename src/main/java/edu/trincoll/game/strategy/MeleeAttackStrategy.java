1c38b228c504c7f9a97cf6716f4e64ec15f07bf1
Ibsa Tassew Geleta <82205002+ibsatassew@users.noreply.github.com>
2025-11-06 22:21:17 -0500

    Update MeleeAttackStrategy.java

--- src/main/java/edu/trincoll/game/strategy/MeleeAttackStrategy.java
+++ src/main/java/edu/trincoll/game/strategy/MeleeAttackStrategy.java
@@ -18,10 +18,4 @@ import edu.trincoll.game.model.Character;
  *   With bonus: 50 * 1.2 = 60
  *   Return: 60
  */
-public class MeleeAttackStrategy implements AttackStrategy {
-    @Override
-    public int calculateDamage(Character attacker, Character target) {
-        // TODO 1a: Implement melee attack calculation
-        throw new UnsupportedOperationException("TODO 1a: Implement melee attack calculation");
-    }
-}
+
